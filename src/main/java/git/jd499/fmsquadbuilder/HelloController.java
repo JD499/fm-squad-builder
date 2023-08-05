2e05f3a63b4891282f7db72a1696dc837a530f07
galileomir <112453386+galileomir@users.noreply.github.com>
2023-08-04 18:15:52 -0700

    Can Add HTML

--- src/main/java/git/jd499/fmsquadbuilder/HelloController.java
+++ src/main/java/git/jd499/fmsquadbuilder/HelloController.java
@@ -2,13 +2,34 @@ package git.jd499.fmsquadbuilder;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import java.io.File;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button fileChooserButton;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onFileChooserButtonClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("HTML Files", "*.html")
+        );
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            welcomeText.setText("File selected: " + selectedFile.getName());
+        } else {
+            welcomeText.setText("File selection cancelled.");
+        }
     }
 }
