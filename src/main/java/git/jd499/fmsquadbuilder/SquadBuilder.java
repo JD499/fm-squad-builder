f8901a0ea5e5b25fb212c6bfe9ffa18e96e42261
galileomir <112453386+galileomir@users.noreply.github.com>
2023-08-09 03:00:58 -0700

    temporary position boxes

--- src/main/java/git/jd499/fmsquadbuilder/SquadBuilder.java
+++ src/main/java/git/jd499/fmsquadbuilder/SquadBuilder.java
@@ -1,10 +1,12 @@
 package git.jd499.fmsquadbuilder;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -19,20 +21,29 @@ import java.io.IOException;
 public class SquadBuilder extends Application {
     @Override
     public void start(Stage primaryStage) {
-        VBox root = new VBox(20);
-        Scene scene = new Scene(root, 600, 800);
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
 
-        // Create the soccer field
-        Pane soccerFieldPane = new Pane();
-        createSoccerField(soccerFieldPane);
+        // Create spacer to push content to the center
+        Region spacerTop = new Region();
+        VBox.setVgrow(spacerTop, Priority.ALWAYS);
 
         // Create the "Choose File" button
         Button fileChooserButton = new Button("Choose File");
         fileChooserButton.setOnAction(event -> onFileChooserButtonClick());
 
-        // Add the button and soccer field to the root layout
-        root.getChildren().addAll(fileChooserButton, soccerFieldPane);
+        // Create the soccer field
+        Pane soccerFieldPane = new Pane();
+        createSoccerField(soccerFieldPane);
 
+        // Create spacer to push content to the center
+        Region spacerBottom = new Region();
+        VBox.setVgrow(spacerBottom, Priority.ALWAYS);
+
+        // Add the spacers, button, and soccer field to the root layout
+        root.getChildren().addAll(spacerTop, fileChooserButton, soccerFieldPane, spacerBottom);
+
+        Scene scene = new Scene(root, 600, 800);
         primaryStage.setTitle("Soccer Field");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -81,6 +92,22 @@ public class SquadBuilder extends Application {
         penaltyAreaBottom.setFill(Color.TRANSPARENT);
         penaltyAreaBottom.setStroke(Color.WHITE);
         pane.getChildren().add(penaltyAreaBottom);
+
+        createPositionBox(pane, 100, 100); // Top-left
+        createPositionBox(pane, 300, 100); // Top-center
+        createPositionBox(pane, 500, 100); // Top-right
+        createPositionBox(pane, 100, 300); // Middle-left
+        createPositionBox(pane, 300, 300); // Middle-center
+        createPositionBox(pane, 500, 300); // Middle-right
+        createPositionBox(pane, 100, 500); // Bottom-left
+        createPositionBox(pane, 300, 500); // Bottom-center
+        createPositionBox(pane, 500, 500); // Bottom-right
+
+
+
+
+
+
     }
 
     protected void onFileChooserButtonClick() {
@@ -117,5 +144,31 @@ public class SquadBuilder extends Application {
 
             return false;
         }
+    }
+
+    private void createPositionBox(Pane pane, double x, double y) {
+        Rectangle positionBox = new Rectangle(x, y, 50, 50);
+        positionBox.setFill(Color.TRANSPARENT);
+        positionBox.setStroke(Color.WHITE);
+
+        // Add drag-and-drop functionality (optional)
+        positionBox.setOnDragOver(event -> onPositionBoxDragOver(event));
+        positionBox.setOnDragDropped(event -> onPositionBoxDragDropped(event));
+
+        pane.getChildren().add(positionBox);
+    }
+
+    private void onPositionBoxDragOver(DragEvent event) {
+        if (event.getGestureSource() != event.getTarget() &&
+                event.getDragboard().hasString()) {
+            event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        }
+        event.consume();
+    }
+
+    private void onPositionBoxDragDropped(DragEvent event) {
+        // Handle the drop, e.g., place the player in the slot
+        // You can access the data using event.getDragboard().getString()
+        event.consume();
     }
 }
